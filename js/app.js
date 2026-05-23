0e96632132854c67026ce6eaae4cfc985f4c9ce7
Litherboar <juanesgonzales7@gmail.com>
2026-05-23 15:49:22 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,50 @@
+/* ACTUALIZACIÓN AUTOMÁTICA */
+
+setInterval(() => {
+
+    console.log("Actualizando estado del sistema...");
+
+}, 30000);
+
+/* LOGIN FRONTEND */
+
+const loginBtn = document.getElementById("login-btn");
+
+if(loginBtn){
+
+    loginBtn.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        window.location.href = "admin.html";
+
+    });
+
+}
+
+/* DETALLE DINÁMICO */
+
+const params = new URLSearchParams(window.location.search);
+
+const sistema = params.get("sistema");
+
+const titulo = document.querySelector(".titulo-sistema");
+
+if(sistema && titulo){
+
+    const nombres = {
+
+        citas:"Sistema de citas médicas",
+
+        historia:"Historia clínica electrónica",
+
+        laboratorio:"API de laboratorio",
+
+        facturacion:"Sistema de facturación",
+
+        servidor:"Servidor de base de datos"
+
+    };
+
+    titulo.innerHTML = nombres[sistema];
+}
