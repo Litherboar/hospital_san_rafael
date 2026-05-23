a2b783f88707211588b6d331f4c6478168897d02
Litherboar <juanesgonzales7@gmail.com>
2026-05-23 17:31:02 -0500

    HRS 15 Actualizar título dinámico en detalle.html via JS
    Desarrollo actualizar título dinámico en detalle.html via JS

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ if(loginBtn){
 }
 
 /* DETALLE DINÁMICO */
-/* Cambio No13. Mapear parámetros a nombres de sistemas (objeto nombres) */
+/* Cambio No14. Mapear parámetros a nombres de sistemas (objeto nombres) */
 
 const params = new URLSearchParams(window.location.search);
 
